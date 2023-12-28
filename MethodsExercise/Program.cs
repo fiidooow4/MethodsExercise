ebe83957d86ad2843ab06cc6f1b060455f99bf33
ilyas Osman <ilyaso.fiiidooow@gmail.com>
2023-12-28 15:38:40 -0600

    new One

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -1,10 +1,52 @@
-﻿namespace MethodsExercise
+﻿using System;
+
+namespace MethodsExercise
 {
-    public class Program
+    internal class Program
     {
+        public static int Sum(int num1, int num2)
+        {
+            var answer = num1 + num2;
+            return answer;
+        }
+
+        public static int Multiply(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var amountOfCars = Sum(2, 6);
+            var blah = Multiply(10, 2);
+
+
+
+            Console.WriteLine("Hello, What is your name?");
+            var userName = Console.ReadLine(); 
+
+            Console.WriteLine($"Hi, {userName}. What is your favorite color?");
+            var color = Console.ReadLine(); 
+
+            Console.WriteLine($"{color} COOL! What is your favorite animal?");
+            var animal = Console.ReadLine(); 
+
+            Console.WriteLine("Great! What is your favorite band?");
+            var band = Console.ReadLine();
+
+            Console.WriteLine($"Thanks, {userName}! Here is your profile");
+            Console.WriteLine("--------------");
+
+            Console.WriteLine($"Name: {userName}");
+            Console.WriteLine($"Favorite Color: {color}");
+            Console.WriteLine($"Favorite Animal: {animal}");
+            Console.WriteLine($"Favorite Band: {band}");
+
+
+
         }
     }
 }
+
+
+
